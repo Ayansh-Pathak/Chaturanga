2096bcdbb6267f42d477225ceea2177a73415461
Ayansh-Pathak <21ks2638@21kschool.school>
2026-08-27 16:14:03 +0000

    new file:   .gradle/9.2.0/checksums/checksums.lock 	new file:   .gradle/9.2.0/fileChanges/last-build.bin 	new file:   .gradle/9.2.0/fileHashes/fileHashes.bin 	new file:   .gradle/9.2.0/fileHashes/fileHashes.lock 	new file:   .gradle/9.2.0/gc.properties 	new file:   .gradle/buildOutputCleanup/buildOutputCleanup.lock 	new file:   .gradle/buildOutputCleanup/cache.properties 	new file:   .gradle/vcs-1/gc.properties 	new file:   build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+id("com.google.gms.google-services") version "4.5.0" apply false
+import("google-services.json")
+id("com.android.application")
+id("com.google.gms.google-services")
+implementation(platform("com.google.firebase:firebase-bom:34.18.0"))
